dbdb663fbf913c4bf58ec37c640f10b25a7fe747
howdyworld <howdy.earth@gmail.com>
2015-08-17 12:18:52 +0300

    inc controls

--- README.md
+++ README.md
@@ -7,5 +7,15 @@
 - 5 - start / pause
 - 6 - set time
 
+(on pause)
+- 3 - plus 10 seconds
+- 2 - plus 1 minute
+- 1 - plus 1 hour
+
+- 9 - minus 10 seconds
+- 8 - minus 1 minute
+- 7 - minus 1 hour
+
+
 #### Dependencies
-- [miso](https://github.com/contextlogger/miso) builds: http://contextlogger.org/miso/download/
+- [miso](https://github.com/contextlogger/miso) - builds: http://contextlogger.org/miso/download/
--- pytimer/pytimerapp.py
+++ pytimer/pytimerapp.py
@@ -28,7 +28,19 @@ class PyTimerApp(Application):
             (u"Set timer", self.set_timer),
             (u"Exit", self.close_app)
         ]
+
+        # key codes and their offsets that change timer
+        self.inc_keys = {
+            key_codes.EScancode3: 10,
+            key_codes.EScancode2: 60,
+            key_codes.EScancode1: 3600,
+            key_codes.EScancode9: -10,
+            key_codes.EScancode8: -60,
+            key_codes.EScancode7: -3600
+        }
+        
         self.draw = Draw()
+        self.controls = self.draw.canvas
         self.start_time = u"00:01:00"
         self.curr_time = self.start_time
         self.pause_flag = False
@@ -48,40 +60,39 @@ class PyTimerApp(Application):
                     self.close_app)
 
     def init_controls(self):
-        self.draw.canvas.bind(key_codes.EScancode4, self.reset)
-        self.draw.canvas.bind(key_codes.EScancode6, self.set_timer)
+        self.controls.bind(key_codes.EScancode4, self.reset)
         self.pause_controls()
 
     def start_controls(self):
         self.menu[0] = self.pause_opt
         self.update_ui()
-        self.draw.canvas.bind(key_codes.EScancode5, self.pause)
-
-    def start(self):
-        if self.curr_time == "00:00:00":
-            return
-
-        self.pause_flag = False
-        self.start_controls()
-        cd_sec = timeutils.to_sec(self.curr_time)
-
-        for curr_sec in reversed(range(cd_sec)):
-            if self.pause_flag:
-                break
-
-            self.curr_time = timeutils.to_str(curr_sec)
-            self.draw_scene()
-            e32.ao_sleep(1)
-
-        if self.curr_time == "00:00:00":
-            miso.vibrate(1000, 50)
-
-        self.pause_controls()
+        self.controls.bind(key_codes.EScancode5, self.pause)
+        self.controls.bind(key_codes.EScancode6, lambda: None)
+        self.disbale_inc_controls()
 
     def pause_controls(self):
         self.menu[0] = self.start_opt
         self.update_ui()
-        self.draw.canvas.bind(key_codes.EScancode5, self.start)
+        self.controls.bind(key_codes.EScancode5, self.start)
+        self.controls.bind(key_codes.EScancode6, self.set_timer)
+        self.enable_inc_controls()
+
+    def enable_inc_controls(self):
+        for key in self.inc_keys:
+            offset = self.inc_keys[key]
+            callback = lambda offset=offset: self.change_time(offset)
+            self.controls.bind(key, callback)
+
+    def disbale_inc_controls(self):
+        for key in self.inc_keys:
+            self.controls.bind(key, lambda: None)
+
+    def change_time(self, offset):
+        sec = timeutils.to_sec(self.curr_time)
+        sec += offset
+        self.start_time = timeutils.to_str(sec)
+        self.curr_time = self.start_time
+        self.draw_scene()
 
     def pause(self):
         self.pause_flag = True
@@ -111,6 +122,27 @@ class PyTimerApp(Application):
                             timeutils.to_sec(self.start_time))
         self.draw.controls()
         self.draw.redraw()
+
+    def start(self):
+        if self.curr_time == "00:00:00":
+            return
+
+        self.pause_flag = False
+        self.start_controls()
+        cd_sec = timeutils.to_sec(self.curr_time)
+
+        for curr_sec in reversed(range(cd_sec)):
+            if self.pause_flag:
+                break
+
+            self.curr_time = timeutils.to_str(curr_sec)
+            self.draw_scene()
+            e32.ao_sleep(1)
+
+        if self.curr_time == "00:00:00":
+            miso.vibrate(1000, 50)
+
+        self.pause_controls()
 
     def close_app(self):
         Application.close_app(self)
